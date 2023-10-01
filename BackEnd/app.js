277d9186cecb696742bb7b3dba09bd1f692e449c
frKnarf <119650431+frKnarf@users.noreply.github.com>
2023-10-01 14:08:58 +0700

    fix

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const commendRoutes = require("./routes/comments");
-const blogDetillRoutes = require("./routes/blogDetill");
+const blogDetailRoutes = require("./routes/blogDetail");
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1/blogs/:id/commends", commendRoutes);
-app.use("/blogDetill", blogDetillRoutes);
+app.use("/blogDetill", blogDetailRoutes);
 
 //Create Server
 app.listen(3000, () => {
