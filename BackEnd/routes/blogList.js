a88fdd72e132684372cc61e59d17b858f036fe87
nattkpk <nattkpk@gmail.com>
2023-10-01 15:49:40 +0700

    10/1/15.50

--- BackEnd/routes/blogList.js
+++ BackEnd/routes/blogList.js
@@ -4,7 +4,48 @@ const { data } = require("../data/blog_data");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json(data);
+  const category = req.query.category;
+  switch (category) {
+    case "sports":
+      const sport = data.filter((blog) => blog.category === "sports");
+      if (sport) {
+        res.json(sport);
+      } else {
+        res.status(404).json({ message: "Sports not found" });
+      }
+      break;
+      
+    case "technology":
+      const technology = data.filter((blog) => blog.category === "technology");
+      if (technology) {
+        res.json(technology);
+      } else {
+        res.status(404).json({ message: "technology not found" });
+      }
+      break;
+
+    case "fashion":
+      const fashion = data.filter((blog) => blog.category === "fashion");
+      if (fashion) {
+        res.json(fashion);
+      } else {
+        res.status(404).json({ message: "fashion not found" });
+      }
+      break;
+
+    case "food":
+      const food = data.filter((blog) => blog.category === "food");
+      if (food) {
+        res.json(food);
+      } else {
+        res.status(404).json({ message: "food not found" });
+      }
+      break;
+
+    default:
+      res.json(data);
+      break;
+  }
 });
 
 // router.get("/:id", (req, res) => {
@@ -29,10 +70,9 @@ router.post("/", (req, res) => {
   res.json(newBlog);
 });
 
-
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const id = Number.parseInt(req.params.id);
-  const index = data.findIndex((blog) => blog.blog_id === id); 
+  const index = data.findIndex((blog) => blog.blog_id === id);
   if (index !== -1) {
     data.splice(index, 1);
     res.status(204).send();
