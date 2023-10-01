84b6519adeaefd6777c88a3c7bc17b3305fd9fc4
nattkpk <nattkpk@gmail.com>
2023-10-01 14:19:47 +0700

    10/1/14.19

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
+const router = require("./routes/blogList");
 const app = express();
 
-const router = require('./routes/products');
-app.use('/api/products', router);
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1/blogs", router);
 
-
-app.listen(3000, () => {
-  console.log(`Listening on port 3000`);
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
--- BackEnd/routes/blogList.js
+++ BackEnd/routes/blogList.js
@@ -1,52 +1,54 @@
-const express = require('express');
-const { data } = require('../data');
+const express = require("express");
+const { data } = require("../data/data");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.json(data);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const id = Number.parseInt(req.params.id);
-  const product = data.find(
-        (product) => product.id === id
-    );
-  res.json(product);
+  const blog = data.find((blog) => blog.blog_id === id);
+  res.json(blog);
 });
 
-router.post('/', (req, res) => {
-  const { name, imageURL, type } = req.body;
-  const newProduct = {
-    id: (data.length + 1),
-    name,
-    imageURL,
-    type
+router.post("/", (req, res) => {
+  const { title, content, type, category, comments } = req.body;
+  const newBlog = {
+    blog_id: data.length + 1,
+    date: new Date().toLocaleDateString(),
+    author: "Your Author Name",
+    title,
+    content,
+    type,
+    category,
+    comments: [],
   };
-  data.push(newProduct);
-  res.json(newProduct);
-});
-
-router.put('/:id', (req, res) => {
-  const { name, imageURL, type } = req.body;
-  const id = Number.parseInt(req.params.id);
-  const upProduct = data.find((
-        product) => product.id === id
-    );
-    upProduct.name = name;
-    upProduct.imageURL = imageURL;
-    upProduct.type = type;
- 
-  res.json(upProduct);
-});
-
-router.delete('/:id', (req, res) => {
-  const id = Number.parseInt(req.params.id);
-  const index = data.findIndex(
-        (product) => product.id === id
-    );
-  data.splice(index, 1);
-  res.status(204).send();
+  data.push(newBlog);
+  res.json(newBlog);
 });
 
 module.exports = router;
+
+// router.put('/:id', (req, res) => {
+//   const { name, imageURL, type } = req.body;
+//   const id = Number.parseInt(req.params.id);
+//   const updateBlog = data.find((
+//         blog) => blog.id === id
+//     );
+//     updateBlog.name = name;
+//     updateBlog.imageURL = imageURL;
+//     updateBlog.type = type;
+
+//   res.json(updateBlog);
+// });
+
+// router.delete('/:id', (req, res) => {
+//   const id = Number.parseInt(req.params.id);
+//   const index = data.findIndex(
+//         (blog) => blog.id === id
+//     );
+//   data.splice(index, 1);
+//   res.status(204).send();
+// });
